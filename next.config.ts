c935796db8abf47326696e9410c475094556f9f2
GovindVyas <vyas.bharvi@gmail.com>
2025-01-16 18:58:51 +0530

    Update Next.js configuration for improved performance and security. Enable standalone output, reactStrictMode, and configure image remote patterns. Disable poweredByHeader for enhanced privacy.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  poweredByHeader: false,
 };
 
 export default nextConfig;
